1b3bd9db01af9dc24b2891b5af7a44f3985b5ef4
KUSH42 <xush@xush.org>
2025-07-19 20:37:01 +0200

    fix: fixed dotenvx import

--- scripts/setup-env.js
+++ scripts/setup-env.js
@@ -1,5 +1,4 @@
 // This script runs before your tests, loading the correct environment file.
 // Note: I'm putting this in a `scripts` directory for organization.
-
-const dotenvx = require('@dotenvx/dotenvx');
-dotenvx.config({ path: './tests/test.env' });
+import { config } from '@dotenvx/dotenvx';
+config({ path: './tests/test.env' });
